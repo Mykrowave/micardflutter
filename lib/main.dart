42a08d2449366fa6acff90fd09e43dccc90a87b7
Mike Parker <mikeparker@crescerance.com>
2019-08-17 23:38:33 -0400

    layoutChallenge

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,36 @@ class HomePage extends StatelessWidget {
       child: MaterialApp(
         home: Scaffold(
           backgroundColor: Colors.teal,
-          body: Container(),
+          body: SafeArea(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  color: Colors.red,
+                  width: 100.0,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100.0,
+                      height: 100.00,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      width: 100.0,
+                      height: 100.00,
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
+                Container(
+                  color: Colors.blue,
+                  width: 100,
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );

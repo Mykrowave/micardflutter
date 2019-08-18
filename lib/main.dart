aad8c443d16cd7975ea5d716c94d3340f9e91ae9
Mike Parker <mikeparker@crescerance.com>
2019-08-17 23:32:44 -0400

    swaptoStatelesswidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+  runApp(HomePage());
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.teal,
+          body: Container(),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }

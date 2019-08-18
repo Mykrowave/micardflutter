2ee367d735327f84cf3863b5d6894df50ff1efb5
Mike Parker <mikeparker@crescerance.com>
2019-08-18 00:14:00 -0400

    backgroundimagecircleavatar

--- lib/main.dart
+++ lib/main.dart
@@ -14,32 +14,18 @@ class HomePage extends StatelessWidget {
         home: Scaffold(
           backgroundColor: Colors.teal,
           body: SafeArea(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Container(
-                  color: Colors.red,
-                  width: 100.0,
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: NetworkImage(
+                      'https://flash.comiccruncher.com/images/characters/b5b85bda.jpg'),
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      width: 100.0,
-                      height: 100.00,
-                      color: Colors.yellow,
-                    ),
-                    Container(
-                      width: 100.0,
-                      height: 100.00,
-                      color: Colors.green,
-                    ),
-                  ],
-                ),
-                Container(
-                  color: Colors.blue,
-                  width: 100,
-                ),
+                SizedBox(
+                  width: double.infinity,
+                )
               ],
             ),
           ),
